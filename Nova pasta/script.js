678f45ccbcf7772351f2f9232bbff90c9d3ad85b
manuribf <158056291+manuribf@users.noreply.github.com>
2024-02-19 11:59:51 -0300

    Add files via upload

---
+++ Nova pasta/script.js
@@ -0,0 +1,48 @@
+let nome = "Manuela"
+let idade = 17
+let estado_civil = false
+console.log(nome)
+console.log(idade)
+console.log(estado_civil)
+
+let nota1 = 10
+let nota2 = 10
+let nota3 = 10
+let nota4 = 10
+
+console.log((nota1 + nota2 + nota3 + nota4) / 4)
+
+n = 5 
+console.log(n, "x 1 =", n * 1)
+console.log(n, "x 2 =", n * 2)
+console.log(n, "x 3 =", n * 3)
+console.log(n, "x 4 =", n * 4)
+console.log(n, "x 5 =", n * 5)
+console.log(n, "x 6 =", n * 6)
+console.log(n, "x 7 =", n * 7)
+console.log(n, "x 8 =", n * 8)
+console.log(n, "x 9 =", n * 9)
+console.log(n, "x 10 =", n * 10)
+
+let valor = 7
+console.log('Sucessor: ', valor + 1)
+console.log('Antecessor: ', valor - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
